fc8185f6ed45674153c84d2ea5eadf84bdcfab3b
Blackbaud-RobCuller <robert.culler@blackbaud.com>
2017-02-18 21:46:49 -0500

    change card.ts in cullermebadd myCardsOnly

--- src/app/card.ts
+++ src/app/card.ts
@@ -4,6 +4,7 @@ export class Card {
     public status: number;
 
     //comment added to cardsOnly branch  
+    //new comment added to cullermebadd myCardsOnly
   
     //constructor(public title: string, public status: number) {}
     constructor(public title: string) {}
