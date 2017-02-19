3d5a8aee33cd5d9a666c7d7adda28619fa457c46
Blackbaud-RobCuller <robert.culler@blackbaud.com>
2017-02-18 20:55:24 -0500

    Update card.ts

--- src/app/card.ts
+++ src/app/card.ts
@@ -3,6 +3,8 @@ export class Card {
     public description: string;
     public status: number;
 
+    //comment added to cardsOnly branch  
+  
     //constructor(public title: string, public status: number) {}
     constructor(public title: string) {}
 }
